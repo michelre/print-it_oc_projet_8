1c59253c5c7a4e3a8ed17ee1d2aae37b900718ae
Rémi Michel <remi.michel38@gmail.com>
2023-07-19 15:39:26 +0200

    feat(): Session du 19/07

--- assets/script.js
+++ assets/script.js
@@ -18,15 +18,49 @@ const slides = [
 
 ]
 
+let currentSlide = 0;
+
+function changeSelectedDot(){
+	const dots = document.querySelectorAll('.dot')
+	dots.forEach((dot, idx) => {
+		dot.classList.remove('dot_selected')
+		if(idx === currentSlide){
+			dot.classList.add('dot_selected')
+		}
+	});
+}
+
+function changeSlide(){
+	const slide = slides[currentSlide]
+	const banner = document.querySelector('.banner-img')
+	const tagLine = document.querySelector('#banner p')
+	banner.setAttribute('src', `./assets/images/slideshow/${slide['image']}`)
+	tagLine.innerHTML = slide['tagLine']
+}
+
+
 const arrow_left = document.querySelector('.arrow_left');
 const arrow_right = document.querySelector('.arrow_right');
 
 arrow_left.addEventListener('click', function() {
-    alert('Clic sur le bouton gauche');
+	if(currentSlide === 0){
+		currentSlide = slides.length - 1
+	} else {
+    	currentSlide -= 1
+	}
+	changeSlide()
+	changeSelectedDot()
 });
 
 arrow_right.addEventListener('click', function() {
-    alert('Clic sur le bouton droit');
+	if(currentSlide === slides.length - 1){
+		currentSlide = 0
+	} else {
+    	currentSlide += 1
+	}
+
+	changeSlide()
+	changeSelectedDot()
 });
 
 
